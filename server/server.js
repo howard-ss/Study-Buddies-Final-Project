f8c0ce21285799d702a19fb2cd9967475ebbc731
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-06-24 21:17:36 +0100

    edit server side
    edit server side to get and post

--- server/server.js
+++ server/server.js
@@ -2,6 +2,22 @@ import http from "node:http";
 import app from "./app";
 import config from "./utils/config";
 import logger from "./utils/logger";
+import { Pool } from "pg";
+
+const pool = new Pool({
+	connectionString: "your-postgres-connection-string",
+});
+
+pool.on("connect", () => {
+	logger.info("Connected to the database");
+});
+
+pool.on("error", (err) => {
+	logger.error("Database connection error:", err);
+});
+
+// Pass the database pool to the app
+app.set("pool", pool);
 
 const server = http.createServer(app);
 
