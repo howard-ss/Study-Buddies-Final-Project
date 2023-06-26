37d011105d197c44fd7465d58da039b2533c0772
laxmi <lkc712628@gmail.com>
2023-06-26 22:28:52 +0100

    create browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import AvailabilityForm from "./AvailabilityForm";
 import MatchingTrainees from "./MatchingTrainees";
 
 const App = () => {
+  const [hasAccount, setHasAccount] = useState(false);
+
   return (
-    <div>
-      <h1>Study Buddies</h1>
+
+		<div>
+      <Header hasAccount={hasAccount} />
+      <Routes>
+         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login setHasAccount={setHasAccount} />} />
+        <Route path="/register" element={<Register setHasAccount={setHasAccount} />} />
+        </Routes>
       <AvailabilityForm />
       <MatchingTrainees />
     </div>
