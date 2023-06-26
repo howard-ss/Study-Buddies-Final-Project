6c2b8ffd40d3bbf998fca3aaa2751958bf2d52bb
laxmi <lkc712628@gmail.com>
2023-06-26 21:17:10 +0100

    made few changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import AvailabilityForm from "./AvailabilityForm";
 import MatchingTrainees from "./MatchingTrainees";
 
 const App = () => {
+  const [hasAccount, setHasAccount] = useState(false);
+
+
   return (
-    <div>
-      <h1>Study Buddies</h1>
+      <div>
+         <Header hasAccount={hasAccount} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login setHasAccount={setHasAccount} />} />
+        <Route path="/register" element={<Register setHasAccount={setHasAccount} />} />
       <AvailabilityForm />
       <MatchingTrainees />
+      </Routes>
     </div>
   );
 };
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Main Page</h1>
+    <p>Choose an option from the navigation menu.</p>
+  </div>
+);
 
 export default App;
