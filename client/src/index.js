b5971aaaa0d5e96f68833405acc21729c84606b7
Howard <howardsoon@gmail.com>
2023-06-27 22:13:11 +0100

    delete public directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,21 @@
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+
+
+// import App from "./App";
+
+// createRoot(document.getElementById("root")).render(
+// 	<BrowserRouter>
+// 		<App />
+// 	</BrowserRouter>
+// );
+
+
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-
 import App from "./App";
-
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
-// const http = require("http");
-// const app = require("./App");
-// const { connectDb, disconnectDb } = require("./db");
-// const config = require("./utils/config");
-// const logger = require("./utils/logger");
-
-// const server = http.createServer(app);
-
-// server.on("listening", () => {
-// 	const addr = server.address();
-// 	const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-// 	logger.info("Listening on: %s", bind);
-// });
-
-// process.on("SIGTERM", () => server.close(() => disconnectDb()));
-
-// connectDb().then(() => server.listen(config.port));
