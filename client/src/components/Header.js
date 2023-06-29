15579da67a9e8dba710eb605e0afa0d1578dddd7
laxmi <lkc712628@gmail.com>
2023-06-29 20:34:08 +0100

    updated header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,37 +4,6 @@ import "./Header.css";
 
 const Header = ({ hasAccount }) => {
    return (
-//     <header>
-//       <nav>
-//         <ul>
-//           <li>
-//             <NavLink to="/" activeclassname="active" exact>
-//               Home
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="/about" activeclassname="active" exact>
-//               About
-//             </NavLink>
-//           </li>
-//           {hasAccount ? (
-//             <li>
-//               <NavLink to="/login" activeclassname="active" exact>
-//                 Login
-//               </NavLink>
-//             </li>
-//           ) : (
-//             <li>
-//               <NavLink to="/register" activeclassname="active" exact>
-//                 Register
-//               </NavLink>
-//             </li>
-//           )}
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
 
 <nav>
 <ul className="navbar">
