5617f67850e8d76c9745ad9d514fb8662739213d
laxmi <lkc712628@gmail.com>
2023-06-29 23:06:46 +0100

    updated login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,35 +1,38 @@
+
 import React, { useState } from "react";
-//import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-     //const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [stayLoggedIn, setStayLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
-    const handleLogin = (e) => {
-      e.preventDefault();
-    //   if (username === "admin" && password === "password") {
-    //     history.push("/");
-    //   } else {
-    //     alert("Invalid username or password.");
-    //   }
-    };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // TODO: Handle login logic
+  //   if (username === "admin" && password === "password") {
+  //     navigate("/");
+  //   } else {
+  //     alert("Invalid username or password.");
+  //   }
+  // };
+  };
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
   return (
     <div className="login-container">
-    <h1>Story Page</h1>
-    {/* <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/my-stories">My Stories</Link></li>
-      </ul>
-    </nav> */}
-
-<main>
+      <h1>Study Buddies Page</h1>
+      <main>
         <section>
           <h2 className="login-title">Login</h2>
           <form className="login-form" onSubmit={handleLogin}>
-            <label htmlFor="login-username" className="login-label">Username</label>
+            <label htmlFor="login-username" className="login-label">
+              Username
+            </label>
             <input
               type="text"
               id="login-username"
@@ -40,7 +43,9 @@ function Login() {
               className="login-input"
             />
 
-            <label htmlFor="login-password" className="login-label">Password</label>
+            <label htmlFor="login-password" className="login-label">
+              Password
+            </label>
             <input
               type="password"
               id="login-password"
@@ -50,8 +55,47 @@ function Login() {
               required
               className="login-input"
             />
+            <Link to="/forgot-password" className="forgot-password-link">
+              Forgot Password?
+            </Link>
+            {/* Stay Logged In checkbox */}
+            <div className="stay-logged-in-container">
+              <input
+                type="checkbox"
+                id="stay-logged-in"
+                name="stay-logged-in"
+                checked={stayLoggedIn}
+                onChange={(e) => setStayLoggedIn(e.target.checked)}
+                className="stay-logged-in-checkbox"
+              />
+              <label htmlFor="stay-logged-in" className="stay-logged-in-label">
+                Stay Logged In
+              </label>
+            </div>
 
-            <button type="submit" className="login-button">Login</button>
+            {/* Login button */}
+            <button type="submit" className="login-button">
+              Login
+            </button>
+
+            {/* Register link */}
+            <div className="register-link-container">
+              <span>Do not have an account? </span>
+              <button
+                type="button"
+                className="register-link"
+                onClick={handleRegister}
+              >
+                Register now
+              </button>
+            </div>
+            <div className="social-login">
+              <button className="social-button">Facebook</button>
+              {/* Example: LinkedIn */}
+              <button className="social-button"> LinkedIn</button>
+              {/* Example: Twitter */}
+              <button className="social-button">Twitter</button>
+            </div>
           </form>
         </section>
       </main>
