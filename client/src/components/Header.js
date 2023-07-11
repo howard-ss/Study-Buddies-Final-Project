6e6fb0f2d13695857fd8985612a54ee64e0737c2
laxmi <lkc712628@gmail.com>
2023-07-11 06:42:07 +0100

    made few small changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,8 +7,8 @@ const Header = ({ hasAccount }) => {
 
 <nav>
 <ul className="navbar">
-        {/* <li><Link to="/">Home</Link></li> */}
-        {/* <li><Link to="/about">About</Link></li> */}
+        <li><Link to="/">Dashboard</Link></li> 
+         <li><Link to="/about">About</Link></li> 
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Register</Link></li>
       </ul>
