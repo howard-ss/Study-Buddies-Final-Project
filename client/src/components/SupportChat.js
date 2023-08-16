01a2e16f07b352e23d1e6f822d606a5baee1debf
howard-ss <howardsoon@gmail.com>
2023-08-16 22:37:02 +0100

    update starter-kit link in fetch of SupportChat.js

--- client/src/components/SupportChat.js
+++ client/src/components/SupportChat.js
@@ -31,7 +31,7 @@ const getMessages = async () => {
       }
 
       try {
-          const response = await fetch("http://localhost:3100/api/completions", options)
+          const response = await fetch("https://starter-kit-66um.onrender.com/", options)
           const data = await response.json()
           setMessage(data.choices[0].message)
       }catch(error){
